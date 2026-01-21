6f68bad7deed7d85ddc1080edb0fd5b89aecc139
josulopez1 <jlopezvelasquez507@gmail.com>
2026-01-20 22:24:24 -0600

    Add basic functionality to RadioPhone class
    Implemented methods to turn on/off the radio and check its status.

--- src/RadioPhone.java
+++ src/RadioPhone.java
@@ -1,2 +1,48 @@
 public class RadioPhone implements IRadio {
 
+    private boolean isOn;
+
+    @Override
+    public void turnOn() {
+        isOn = true;
+    }
+
+    @Override
+    public void turnOff() {
+        isOn = false;
+    }
+
+    @Override
+    public boolean isOn() {
+        return isOn;
+    }
+
+    // ---- Métodos pendientes ----
+
+    @Override
+    public void switchBand() {}
+
+    @Override
+    public String getBand() {
+        return "";
+    }
+
+    @Override
+    public void nextFrequency() {}
+
+    @Override
+    public double getCurrentFrequency() {
+        return 0.0;
+    }
+
+    @Override
+    public void saveStation(int button) {}
+
+    @Override
+    public void selectButton(int button) {}
+
+    @Override
+    public double getButtonFrequency(int button) {
+        return 0.0;
+    }
+}
